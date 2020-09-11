19a6a364f71d3cbc7d734fca5d301830973c31fb
Jonathan Baecker <jb@pixelcrusher.de>
2020-09-11 11:24:32 +0200

    format stdout logging

--- ffplayout-engine/assets/socket-server.py
+++ ffplayout-engine/assets/socket-server.py
@@ -1,47 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
 import signal
 import socket
+import sys
 from subprocess import call, check_output
 
 import psutil
+
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(filename)s: %(message)s')
+logger = logging.getLogger('socket-server')
 
 
 def main(sock):
     while True:
         connection, client_address = sock.accept()
         try:
-            print('connection from', client_address)
+            logger.debug('connection from: {}'.format(client_address))
 
             # Receive the data in small chunks and retransmit it
             while True:
                 data = connection.recv(7)
 
                 if data == b'start':
-                    print('start ffplayout-engine')
+                    logger.debug('starting ffplayout-engine...')
                     call(['supervisorctl', 'start', 'ffplayout-engine'])
                     connection.send(b'200')
                 if data == b'stop':
-                    print('stop ffplayout-engine')
+                    logger.debug('stopping ffplayout-engine...')
                     call(['supervisorctl', 'stop', 'ffplayout-engine'])
                     connection.send(b'200')
                 if data == b'restart':
-                    print('restart ffplayout-engine')
+                    logger.debug('restarting ffplayout-engine...')
                     call(['supervisorctl', 'restart', 'ffplayout-engine'])
                     connection.send(b'200')
                 if data == b'reload':
-                    print('reload ffplayout-engine')
+                    logger.debug('reloading ffplayout-engine...')
                     for proc in psutil.process_iter():
                         if 'ffplayout.py' in proc.cmdline():
                             proc.send_signal(signal.SIGHUP)
                     connection.send(b'200')
                 if data == b'status':
-                    print('get status from ffplayout-engine')
+                    logger.debug('get status from ffplayout-engine...')
                     status = check_output(
                         ['supervisorctl', 'status', 'ffplayout-engine'])
 
-                    status = status.split()[1].encode()
+                    status = status.split()[1]
 
                     connection.send(status)
                 else:
@@ -57,7 +64,7 @@ if __name__ == '__main__':
 
     # Bind the socket to the port
     server_address = ('0.0.0.0', 64233)
-    print('Starting up on {} port {}'.format(*server_address))
+    logger.info('Starting up on {} port {}'.format(*server_address))
     sock.bind(server_address)
 
     # Listen for incoming connections
